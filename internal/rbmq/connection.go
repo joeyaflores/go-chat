4696dd06d19b9b4a7b2ebe353a56aac1fa987052
Joey Flores <joey.flores@tcu.edu>
2024-01-06 12:35:07 -0600

    add echo webserver and rbmq

---
+++ internal/rbmq/connection.go
@@ -0,0 +1,54 @@
+package rbmq
+
+import (
+  "log"
+  "fmt"
+  "os"
+  "os/signal"
+  "syscall"
+  rabbitmq "github.com/wagslane/go-rabbitmq"
+ ) 
+
+func Start() {
+  conn, err := rabbitmq.NewConn(
+    "amqp://user:password@localhost",
+    rabbitmq.WithConnectionOptionsLogging,
+  )
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer conn.Close()
+
+  consumer, err := rabbitmq.NewConsumer(
+    conn,
+    func(d rabbitmq.Delivery) rabbitmq.Action {
+      log.Printf("consumed: %v", string(d.Body))
+       // rabbitmq.Ack, rabbitmq.NackDiscard, rabbitmq.NackRequeue
+      return rabbitmq.Ack
+    },
+    "my_queue",
+    rabbitmq.WithConsumerOptionsRoutingKey("my_routing_key"),
+    rabbitmq.WithConsumerOptionsExchangeName("events"),
+    rabbitmq.WithConsumerOptionsExchangeDeclare,
+  )
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer consumer.Close()
+  // block main thread - wait for shutdown signal
+  sigs := make(chan os.Signal, 1)
+  done := make(chan bool, 1)
+
+  signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+  go func() {
+    sig := <-sigs
+    fmt.Println()
+    fmt.Println(sig)
+    done <- true
+  }()
+
+  fmt.Println("awaiting signal")
+  <-done
+  fmt.Println("stopping consumer")
+}
